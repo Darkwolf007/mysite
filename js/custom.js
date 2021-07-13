11a48c3d056c60bfef4bba153fedfa06558d0d0d
kg11 <krishna.guru09@gmail.com>
2021-07-13 17:20:14 +0530

    added dialog box

--- js/custom.js
+++ js/custom.js
@@ -6,4 +6,17 @@
  * Version: v1.1.0
  * License: Themeforest Standard Licenses: https://themeforest.net/licenses
  */
-!function(n){"use strict";n((function(){})),n(window).on("load",(function(){}))}(jQuery);
+!(function (n) {
+    "use strict";
+    n(function () {}), n(window).on("load", function () {});
+})(jQuery);
+
+$(".modal").on("shown.bs.modal", function (event) {
+    let el = document.getElementsByClassName("close-btn")[0];
+    if (el) el.style.display = "none";
+});
+
+$(".modal").on("hiden.bs.modal", function (event) {
+    let el = document.getElementsByClassName("close-btn")[0];
+    if (el) el.style.display = "flex";
+});
